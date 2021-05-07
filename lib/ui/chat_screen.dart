93d4ed1fb70605324bf718c8f92fdd90d6facd90
andrade-felipe-dev <andradefelipedev@gmail.com>
2021-05-07 08:23:54 -0300

    Google Auth
    Add Google Auth and data of user in message list

--- lib/ui/chat_screen.dart
+++ lib/ui/chat_screen.dart
@@ -2,9 +2,13 @@ import 'dart:io';
 
 import 'package:chat/ui/text_composer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
+
+import 'chat_message.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -13,9 +17,63 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
 
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey<ScaffoldState>();
+
+  FirebaseUser _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.onAuthStateChanged.listen(
+      (user) { 
+        _currentUser = user;
+      }
+    );
+  }
+
+
+  Future <FirebaseUser> _getUser() async {
+    if(_currentUser != null) return _currentUser;
+
+    try{
+      final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleSignInAuthentication.idToken, 
+        accessToken: googleSignInAuthentication.accessToken
+      );
+
+      final AuthResult authResult = await FirebaseAuth.instance.signInWithCredential(credential);
+
+      final FirebaseUser user = authResult.user;
+
+      return user;
+    } catch (error) {
+      return null;
+    }
+  }
+
+
   void _sendMessage({String text, PickedFile imgPick}) async {
 
-    Map<String, dynamic> data = {};
+    final FirebaseUser user = await _getUser();
+
+    if(user == null) {
+      _scaffoldkey.currentState.showSnackBar(
+        SnackBar(
+          content: Text('Não foi possível fazer o login, tente fazer novamente.'),
+          backgroundColor: Colors.red,
+        )
+      );
+    }
+
+
+    Map<String, dynamic> data = {
+      "uid" : user.uid,
+      "senderName": user.displayName,
+      "senderPhotoUrl" : user.photoUrl
+    };
 
 
     if(imgPick != null) {
@@ -35,20 +93,48 @@ class _ChatScreenState extends State<ChatScreen> {
     if(text != null) data["text"] = text;
 
 
-    Firestore.instance.collection("mensagens").add({
-      "text" : text
-    });
+    Firestore.instance.collection("mensagens").add(data);
   }
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldkey,
       appBar: AppBar(
         title: Text("Olá"),
         elevation: 0,
       ),
-      body: TextComposer(_sendMessage),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance.collection('mensagens').snapshots(),
+              builder: (context, snapshot) {
+                switch(snapshot.connectionState) {
+                  case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                  default:
+                    List<DocumentSnapshot> documents = snapshot.data.documents;
+                      snapshot.data.documents.reversed.toList();
+
+                  return ListView.builder(
+                    itemCount: documents.length,
+                    reverse: true,
+                    itemBuilder: (context, index) {
+                      return ChatMessage(documents[index].data, true);
+                    }
+                  );
+                }
+              },
+            ),
+          ),
+          TextComposer(_sendMessage),
+        ],
+      ),
     );
   }
 }
